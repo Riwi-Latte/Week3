317355fefdb31ed62e913641a8bbdee22e1acacf
isabellaPulgarin <isabellapul7@gmail.com>
2025-09-22 18:19:55 -0500

    feat: create the class Animal with attribute name and  method makeSound and method get for name

---
+++ Review/Animal.java
@@ -0,0 +1,15 @@
+package Review;
+
+public class Animal {
+    
+    String name;
+
+    public String makeSound(){
+        return "Hacer sonido";
+    }
+
+    public String getName(){
+        return name;
+    }
+
+}
