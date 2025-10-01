1b59f1c0788d356292963288e75b6a6e0097ca56
Oomass7 <loaizarodriueztomas@gmail.com>
2025-09-30 21:38:41 -0500

    Revert "feat: add features 1 & 2 of the activity review"
    This reverts commit c90e9c86cd841dc3e1b1acb88cc98bbef362300f.
    
    changes revert in branch main

--- Review/Cat.java
+++
@@ -1,8 +0,0 @@
-public class Cat extends Animal{
-    
-    @Override
-    public void makeSound() {
-        // TODO Auto-generated method stub
-        System.out.println("Meow");
-    }
-}
--- Review/Dog.java
+++
@@ -1,9 +0,0 @@
-public class Dog extends Animal{
-
-    @Override
-    public void makeSound() {
-        // TODO Auto-generated method stub
-        System.out.println("Woof");
-    }
-
-}
